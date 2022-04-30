4b311b61dcc1930a7871cb133020e73a38426962
Baldwin Suen <36251321+baldwinsuen@users.noreply.github.com>
2022-04-30 11:43:28 -0400

    Initial Commit

---
+++ PongScreenSaver/PongScreenSaverView.h
@@ -0,0 +1,12 @@
+//
+//  PongScreenSaverView.h
+//  PongScreenSaver
+//
+//  Created by Baldwin Suen on 4/30/22.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface PongScreenSaverView : ScreenSaverView
+
+@end
